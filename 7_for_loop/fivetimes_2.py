e68a25b58fa8a5d065b64a32bd1c580fe65dacb0
himangshupal719 <pal.h719@gmail.com>
2020-12-01 16:16:43 -0800

    Completed lesson 7

---
+++ 7_for_loop/fivetimes_2.py
@@ -0,0 +1,18 @@
+print('My name is')
+for  i in range(12,16):
+    print('Jimmy Five Times ' + str(i))
+
+print("############")
+
+print('My name is')
+for  i in range(0, 10, 2):
+    print('Jimmy Five Times ' + str(i))
+
+
+
+print("############")
+
+print('My name is')
+for  i in range(5, -1,-1):
+    print('Jimmy Five Times ' + str(i))
+
