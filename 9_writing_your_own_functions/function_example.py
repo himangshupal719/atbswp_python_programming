7f15ae99ec1714cc0799b3df08df19c44573dfd4
himangshupal719 <pal.h719@gmail.com>
2020-12-08 07:33:49 -0800

    Completed lesson 9

---
+++ 9_writing_your_own_functions/function_example.py
@@ -0,0 +1,10 @@
+def hello():
+    print('Howdy!')
+    print('Howdy!!!')
+    print('Hello there.')
+
+
+# Function call - 3 times
+hello()
+hello()
+hello()
---
+++ 9_writing_your_own_functions/print_new_line.py
@@ -0,0 +1,6 @@
+print('Hello')
+print('World')
+
+print('Hello', end = '')  #end is the keyword argument. 
+print('World')
+
